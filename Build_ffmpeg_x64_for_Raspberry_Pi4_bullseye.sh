6d260c7407dfda53e155b55bac47cd0168246131
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-27 21:15:21 +1030

    Add files via upload

--- Build_ffmpeg_x64_for_Raspberry_Pi4_bullseye.sh
+++ Build_ffmpeg_x64_for_Raspberry_Pi4_bullseye.sh
@@ -1061,7 +1061,7 @@ export CPPFLAGS=" -O3 -fstack-protector-all -D_FORTIFY_SOURCE=2 -I/usr/local/inc
 export LDFLAGS=" -O3 -fstack-protector-all -D_FORTIFY_SOURCE=2 -I/usr/local/include -I/usr/include/aarch64-linux-gnu -I/usr/include -L/usr/local/lib -L/usr/lib/aarch64-linux-gnu -L/usr/lib "
 ./configure \
 	--extra-version="ffmpeg_for_RPi4B_having_h264_v4l2m2m" \
-	--arch=aarch64 \
+	--arch=aarch64 --target-os=linux \
 	--disable-shared --enable-static --enable-pic --enable-neon --disable-w32threads --enable-pthreads \
 	--enable-gpl --enable-version3 --enable-nonfree \
 	--prefix=/usr/local \
