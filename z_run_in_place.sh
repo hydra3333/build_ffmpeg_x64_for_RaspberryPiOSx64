39fcec8eb45a24edd5357b434dfe7ed77c0b7917
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-08 20:53:57 +1030

    Add files via upload

--- z_run_in_place.sh
+++ z_run_in_place.sh
@@ -75,7 +75,18 @@ rm -fv ff_CBR.log
 
 mediainfo -full "./some_test_input_file_tiny_transcoded_h264_v4l2m2m.mp4" 2>&1 >> ff_CBR.log
 /usr/local/bin/ffmpeg -hide_banner -encoders 2>&1 >> ff_CBR.log
+/usr/local/bin/ffmpeg -hide_banner -encoders | grep v4l2
+# V..... h263_v4l2m2m         V4L2 mem2mem H.263 encoder wrapper (codec h263)
+# V..... h264_v4l2m2m         V4L2 mem2mem H.264 encoder wrapper (codec h264)
+# V..... hevc_v4l2m2m         V4L2 mem2mem HEVC encoder wrapper (codec hevc)
+# V..... mpeg4_v4l2m2m        V4L2 mem2mem MPEG4 encoder wrapper (codec mpeg4)
+# V..... vp8_v4l2m2m          V4L2 mem2mem VP8 encoder wrapper (codec vp8)
+
 /usr/local/bin/ffmpeg -hide_banner -h encoder=h264_v4l2m2m 2>&1 >> ff_CBR.log
+/usr/local/bin/ffmpeg -hide_banner -h encoder=hevc_v4l2m2m 2>&1 >> ff_CBR.log
+/usr/local/bin/ffmpeg -hide_banner -h encoder=mpeg4_v4l2m2m 2>&1 >> ff_CBR.log
+/usr/local/bin/ffmpeg -hide_banner -h encoder=vp8_v4l2m2m 2>&1 >> ff_CBR.log
+/usr/local/bin/ffmpeg -hide_banner -h encoder=h263_v4l2m2m 2>&1 >> ff_CBR.log
 
 
 
