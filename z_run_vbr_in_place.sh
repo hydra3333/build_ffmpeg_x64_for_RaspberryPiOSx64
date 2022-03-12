4f5d1d6321c7d8ed3e28c93fc8a34f6472cb219a
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-12 10:35:42 +1030

    Add files via upload

--- z_run_vbr_in_place.sh
+++ z_run_vbr_in_place.sh
@@ -5,6 +5,26 @@
 set -x
 cd ~/Desktop
 
+
+rm -fv ff_VBR_std.log
+/usr/bin/ffmpeg -hide_banner -nostats -v trace \
+		-i "./some_test_input_file_tiny.mp4" \
+		-vsync cfr \
+		-sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp \
+		-strict experimental \
+		-filter_complex "[0:v]yadif=0:0:0,format=pix_fmts=yuv420p" \
+		-c:v h264_v4l2m2m \
+		-pix_fmt yuv420p \
+		-b:v 4000000 \
+		-profile:v 4 \
+		-level 4.2 \
+		-g:v 25 \
+		-movflags +faststart+write_colr \
+		-an \
+		-y "./some_test_input_file_tiny_transcoded_h264_VBR_std_v4l2m2m.mp4" 2>&1 | tee ff_VBR_std.log
+
+echo "************************************************************************************************************************"
+
 rm -fv ff_VBR.log
 /usr/local/bin/ffmpeg -hide_banner -nostats -v trace \
 		-i "./some_test_input_file_tiny.mp4" \
